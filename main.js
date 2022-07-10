fa0613c9f4f3fd1da5d4bbf872b8c94ddc481a28
janko12 <henrich.sury97@gmail.com>
2022-07-10 16:36:06 +0200

    Able to resize grid and close modal

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
 const dialog = document.querySelector("dialog");
 const grid = document.querySelector(".grid");
 const gridSize = grid.clientWidth;
+const resizeButton = document.querySelector(".resize-button");
+const closeButton = document.querySelector(".close-button");
+const form = document.forms[0];
+const gridSizeInput = form.querySelector("input");
+const gridSizeLimit = 100;
 
-function loadGrid(gridSizeInSquares) {
-    if (gridSizeInSquares <= 0) {
-        return;
-    }
-    const squareSize = gridSize / gridSizeInSquares;
-
-    for (let i = 0; i < gridSizeInSquares ** 2; i++) {
-        const square = makeSquare(squareSize);
-        grid.appendChild(square);
-    }
-}
+resizeButton.addEventListener("click", showModal);
+closeButton.addEventListener("click", closeModal);
+form.addEventListener("submit", (evt) => {
+    loadGrid(+gridSizeInput.value);
+});
 
 function makeSquare(size) {
     const square = document.createElement("div");
     square.style.width = size + "px";
     square.style.height = size + "px";
     return square;
+}
+
+function loadGrid(gridSizeInSquares) {
+    if (typeof gridSizeInSquares !== "number" || gridSizeInSquares <= 0) {
+        return;
+    }
+    if (gridSizeInSquares > gridSizeLimit) {
+        gridSizeInSquares = gridSizeLimit;
+    }
+    const squareSize = gridSize / gridSizeInSquares;
+
+    clearGrid();
+    for (let i = 0; i < gridSizeInSquares ** 2; i++) {
+        const square = makeSquare(squareSize);
+        grid.appendChild(square);
+    }
+}
+
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+function showModal() {
+    dialog.showModal();
+}
+
+function closeModal() {
+    dialog.close();
 }
 
 loadGrid(16);
